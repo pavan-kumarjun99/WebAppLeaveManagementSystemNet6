67f7b1f3b2c24cb39f8cfcaca7890d9f9c6a0cc8
Pavan Kumar R <pavankumarjun99@gmail.com>
2024-04-12 10:49:20 +0530

    APR 12

--- WebAppLeaveManagementSystem/Data/ApplicationDbContext.cs
+++ WebAppLeaveManagementSystem/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WebAppLeaveManagementSystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
